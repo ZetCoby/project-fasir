c1b4e648feff918a10c9fa8f32cd641ca624e30e
Cosmin Ioan Seviciu <cosmin.seviciu@3pillarglobal.com>
2019-08-30 14:55:07 +0300

    fetch 20 story and show them

--- src/App.js
+++ src/App.js
@@ -4,27 +4,41 @@ import './App.css';
 
 
 class App extends Component {
-
   state = {
-    storyList: []
+    topStoriesIds: [],
+    storyList: [],
   }
   
+  getItems() {
+    const requests = [];
+    for (let i = 0; i <= 20; i++) {
+      requests.push(fetch(`https://hacker-news.firebaseio.com/v0/item/${this.state.topStoriesIds[i]}.json`).then(res => res.json()));
+    }
+    Promise.all(requests)
+      .then(data => {
+        this.setState({storyList: data})
+      }).catch(console.log) 
+  }
+
   componentDidMount() {
     fetch('https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty')
     .then(res => res.json())
     .then(data => {
-      const storyList = data.map(id => {
-        return <Story id={id}></Story>
-      })
-      this.setState({storyList});
-    })
-    .catch(console.log)
+      this.setState({ topStoriesIds: data});
+      this.getItems();
+    }).catch(console.log)
+  }
+
+  showStories() {
+    return this.state.storyList.map((story, i) => {
+      return <Story story={story} key={i}></Story>
+    });
   }
 
   render() {
     return (
-        <div className="app">
-          {this.state.storyList}
+        <div className="app container-fluid">
+          {this.showStories()}
         </div>
       );
     }
--- src/components/story/story.js
+++ src/components/story/story.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './story.css';
 
 export default class Story extends React.Component {
 
     render(){
       return (
-        <div className="story">{this.props.id}</div>
+        <div className="story-wrapper">
+          <div className="points">
+            {this.props.story.score}
+          </div>
+          <div className="content">
+            <div className="title">
+              {this.props.story.title}  
+            </div>
+            <div className="details">
+              {this.props.story.by}  
+            </div>
+          </div>
+          
+        </div>
       );
     }
 }
