019d6b8e18914e45ec5d4675868528bc56331ee1
Cosmin Ioan Seviciu <cosmin.seviciu@3pillarglobal.com>
2019-10-18 14:46:00 +0300

    added some swag

--- src/components/story/story.js
+++ src/components/story/story.js
@@ -7,14 +7,14 @@ export default class Story extends React.Component {
       return (
         <div className="story-wrapper">
           <div className="points">
-            {this.props.story.score}
+            {this.props.story.score} P
           </div>
           <div className="content">
             <div className="title">
               {this.props.story.title}  
             </div>
             <div className="details">
-              {this.props.story.by}  
+              {this.props.story.by} | {this.props.story.kids ? this.props.story.kids.length : 0} comments
             </div>
           </div>
           
