bd2918f87b0aa9f5df6eb2a5c180d612b066f990
Cosmin Ioan Seviciu <cosmin.seviciu@3pillarglobal.com>
2019-11-08 16:03:43 +0200

    Updates

---
+++ precache-manifest.b6477e24b8c6622e2f273e923513b6d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e76fb35e246c9dd89a03de09f478cc97",
+    "url": "/project-fasir/index.html"
+  },
+  {
+    "revision": "f069d4467dd7bd5e1952",
+    "url": "/project-fasir/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "b67b0e1803769b162834",
+    "url": "/project-fasir/static/css/main.57ac1925.chunk.css"
+  },
+  {
+    "revision": "f069d4467dd7bd5e1952",
+    "url": "/project-fasir/static/js/2.a1879a1d.chunk.js"
+  },
+  {
+    "revision": "b67b0e1803769b162834",
+    "url": "/project-fasir/static/js/main.f563e3de.chunk.js"
+  },
+  {
+    "revision": "e0f9cab623fd7380261c",
+    "url": "/project-fasir/static/js/runtime~main.2345d1aa.js"
+  }
+]);
