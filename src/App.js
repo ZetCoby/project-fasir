3e0eeaaa743029c98e14e4a3832d2e40e327f34a
Cosmin Ioan Seviciu <cosmin.seviciu@3pillarglobal.com>
2019-11-08 16:10:08 +0200

    changed the route to use hash

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import StoryView from './components/story-view/story-view';
 import Home from './components/home/home';
 import './App.css';
