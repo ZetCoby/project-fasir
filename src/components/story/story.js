394f6cd499ec8e15c45c35c3a9dd2d7858527f55
Cosmin Ioan Seviciu <cosmin.seviciu@3pillarglobal.com>
2019-11-08 16:31:48 +0200

    new cool stateless component, Help from Adi

--- src/components/story/story.js
+++ src/components/story/story.js
@@ -2,26 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './story.css';
 
-export default class Story extends React.Component {
-
-    render(){
-      return (
-        <div className="story-wrapper">
-          <Link to={`/story/${this.props.story.id}`}>
-            <div className="points">
-              {this.props.story.score} P
-            </div>
-            <div className="content">
-              <div className="title">
-                {this.props.story.title}  
-              </div>
-              <div className="details">
-                {this.props.story.by} | {this.props.story.kids ? this.props.story.kids.length : 0} comments
-              </div>
-            </div>
-          </Link>
-          
+export default function Story({ story }) {
+  return (
+    <div className="story-wrapper">
+      <Link to={`/story/${story.id}`}>
+        <div className="points">{story.score} P</div>
+        <div className="content">
+          <div className="title">{story.title}</div>
+          <div className="details">
+            {story.by}|{story.kids ? story.kids.length : 0}
+            comments
+          </div>
         </div>
-      );
-    }
+      </Link>
+    </div>
+  );
 }
